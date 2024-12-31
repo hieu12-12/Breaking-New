eab3e6566a1b245e84b738681adf75590f2090a0
Dolo-Devs <shawcrystal29@gmail.com>
2024-12-31 16:32:00 -0600

    BlogList, BlogItem, and Footer components

--- app/page.js
+++ app/page.js
@@ -1,8 +1,13 @@
+'use client';
+import BlogList from '@/Components/BlogList';
+import Header from '@/Components/Header';
 
 export default function Home() {
   return (
     <>
-    
+    <Header />
+    <BlogList />
+    <Footer/>
     </>
   );
 }
