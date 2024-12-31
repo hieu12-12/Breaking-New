cd0dce83556b32205efd5d8e1b3c02ea3928c5e7
hon chau <chaugeorge9@gmail.com>
2024-12-31 14:04:18 -0600

    added component header

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -1,3 +1,4 @@
+import {assests} from '@/Assets/assets'
 import Image from 'next/image'
 import React from 'react'
 
@@ -8,7 +9,7 @@ const Header = () => {
                 <Image src={assets.logo} width={180} alt='' className='w-[130px] sm:w-auto'/>
                 <button className='flex items-center gap-2 font-medium py-1 px-3 sm:px-3 sm:px-6 border border-solid border-black shadow-[-7px_7px_0px_#000000]'>Get Started<Image src={assets.arrow}/></button>
             </div>
-            <div className='text-center my-8 '>
+            <div className='text-center my-8'>
                 <h1 className='text-3xl sm:text-5xl font-medium'>Latest Blogs</h1>
                 <p className="mt-10 max-w-[740px] m-auto text-xs sm:text-base">Lorem Ipsum is simply dummy text of the printing and typ</p>
                 <form className="flex justify-between max-w-[500px] scale-75 sm:scale-100 mx-auto mt-10 border border-black shadow-[-7px_7px_0px_#000000]" action="">
@@ -18,6 +19,5 @@ const Header = () => {
             </div>
         </div>
     )
-
 }
 export default Header;
